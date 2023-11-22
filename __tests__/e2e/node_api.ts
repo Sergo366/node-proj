2a236ae01b9daa26339be834bd66e1dd4f795c1f
Kozenkov <s.kozenkov@Sagax.Software>
2023-11-22 09:24:33 +0200

    Added tests and endpoints

--- __tests__/e2e/node_api.ts
+++ __tests__/e2e/node_api.ts
@@ -2,9 +2,93 @@ import request from "supertest"
 import {app} from "../../src";
 
 describe("/courses", () => {
+    beforeAll(async () => {
+        await request(app).delete("/__test__/data")
+    })
+
     it('should return 200 and empty array', async function () {
         await request(app)
             .get('/courses')
+            .expect(200, [])
+    });
+
+    it('should return 404 for not existing course', async function () {
+        await request(app)
+            .get('/courses/999')
+            .expect(404)
+    });
+
+    it('should\'nt create course with incorrect input data', async () =>  {
+        await request(app)
+            .post("/courses")
+            .send('')
+            .expect(400)
+
+        await request(app)
+            .get('/courses')
+            .expect(200, [])
+    });
+
+    let createdCourse: any = null;
+
+    it('should create course with correct input data', async () =>  {
+        const response = await request(app)
+            .post("/courses")
+            .send({title: "devops"})
+            .expect(200)
+
+        createdCourse = response.body;
+
+        expect(createdCourse).toEqual({
+            id: expect.any(Number),
+            title: "devops"
+        })
+
+        await request(app)
+            .get('/courses')
+            .expect(200, [createdCourse])
+    });
+
+    it('should"nt update course with incorrect input data', async () =>  {
+        await request(app)
+            .put(`/courses/${createdCourse.id}`)
+            .send({title: ""})
+            .expect(400)
+
+        await request(app)
+            .get(`/courses/${createdCourse.id}`)
+            .expect(200, createdCourse)
+    });
+
+    it('should"nt update course that not exist', async () =>  {
+        await request(app)
+            .put(`/courses/-100`)
+            .send({title: "Java"})
+            .expect(404)
+    });
+
+    it('should update course with correct input model', async () =>  {
+        await request(app)
+            .put(`/courses/${createdCourse.id}`)
+            .send({title: "Node JS"})
+            .expect(204)
+
+        await request(app)
+            .get(`/courses/${createdCourse.id}`)
+            .expect(200, {... createdCourse, title: "Node JS"})
+    });
+
+    it('should delete course', async () =>  {
+        await request(app)
+            .delete(`/courses/${createdCourse.id}`)
+            .expect(204)
+
+        await request(app)
+            .get(`/courses/${createdCourse.id}`)
+            .expect(404)
+
+        await request(app)
+            .get(`/courses`)
             .expect(200, [])
     });
 })
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,78 @@
 import express, {Request, Response} from "express"
 
 export const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3001
+
+const jsonBody = express.json()
+app.use(jsonBody)
+
+const db = {
+    courses: [
+        {id: 0, title: "Frontend"},
+        {id: 1, title: "Backend"},
+    ]
+}
 
 app.get('/', (req: Request, res: Response) => {
     res.send("Hello Harismatic")
 })
 
 app.get('/courses', (req: Request, res: Response) => {
-    res.json([])
+    res.json(db.courses)
 })
 
+app.get('/courses/:id', (req: Request, res: Response) => {
+    const requestId = +req.params.id
+    const foundCourse = db.courses.find(i => i.id === requestId)
+
+    if (!requestId || !foundCourse) {
+        res.sendStatus(404)
+        return
+    }
+
+    res.json(foundCourse)
+})
+
+app.post('/courses', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400)
+        return
+    }
+
+    const newCourse = {
+        id: +new Date(),
+        title: req.body.title
+    }
+    db.courses.push(newCourse)
+    res.json(newCourse)
+})
+
+app.put('/courses/:id', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400)
+        return
+    }
+
+    const foundCourse = db.courses.find(el => el.id === +req.params.id)
+    if (!foundCourse) {
+        res.sendStatus(404)
+        return
+    }
+
+    foundCourse.title = req.body.title
+    res.sendStatus(204)
+})
+
+app.delete('/__test__/data', (req, res) => {
+    db.courses = []
+    res.sendStatus(204)
+})
+
+app.delete('/courses/:id', (req, res) => {
+    db.courses = db.courses.filter(el => el.id !== +req.params.id)
+
+    res.sendStatus(204)
+})
 
 app.listen(port, () => {
     console.log("Listen node js")
